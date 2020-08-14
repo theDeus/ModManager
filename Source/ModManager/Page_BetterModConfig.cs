ac90cd2faa8f6af46a5efc74f5a790bfda67cc32
theDeus <vakudva@gmail.com>
2020-08-14 00:49:21 -0700

    Pull out PreviewImagePath as ModInfo property.

--- Source/ModManager/Page_BetterModConfig.cs
+++ Source/ModManager/Page_BetterModConfig.cs
@@ -471,12 +471,9 @@ namespace ModManager
         {
             try
             {
-                string previewImagePath = (mod.Active) ? 
-                    mod.versions.Find(m => m.active).ModMeta.PreviewImagePath : 
-                    mod.versions[0].ModMeta.PreviewImagePath;
                 //Texture size doesn't matter. Resizes on loadImage.
                 Texture2D texture = new Texture2D(2, 2);
-                texture.LoadImage(System.IO.File.ReadAllBytes(previewImagePath));
+                texture.LoadImage(System.IO.File.ReadAllBytes(mod.PreviewImagePath));
 
                 GUI.DrawTexture(previewImage, texture, ScaleMode.ScaleToFit);
             }
